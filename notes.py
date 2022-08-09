34496c1322761f831fa76a61602bbc72ca1d1ed4
rafaelmgbh <rafaelsantosdearaujo@gmail.com>
2022-08-09 07:37:28 -0300

    change file name

---
+++ notes.py
@@ -0,0 +1,15 @@
+"""Bloco de notas
+
+$notes.py new "Minha Nota"
+tag: tech
+text: 
+Anotação geral sobre carreira de tecnologia 
+
+$ notes.py read --tag=tech
+
+...
+...
+"""
+__version__ = "0.1.0"
+
+
